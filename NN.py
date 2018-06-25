b029d73dc8085705e982506a90c175778bd7723e
Kristy WU <34116818+Kristytiki@users.noreply.github.com>
2018-06-25 10:14:01 -0700

    Add files via upload

---
+++ NN.py
@@ -0,0 +1,194 @@
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from nn.classifiers.fc_net import *
+from nn.data_utils import get_mnist_data
+from nn.gradient_check import eval_numerical_gradient, eval_numerical_gradient_array
+from nn.solver import Solver
+from nn.layers import *
+#import os
+
+#os.chdir("/Users/wuzheqi/Documents/2018/Spring/204/final/code")
+
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+def rel_error(x, y):
+    """ returns relative error """
+    return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
+
+# Load the (preprocessed) MNIST data.
+# The second dimension of images indicated the number of channel. For black and white images in MNIST, channel=1.
+data = get_mnist_data()
+for k, v in list(data.items()):
+    print(('%s: ' % k, v.shape))
+
+#############################################################################################################
+# TODO (1) Open the file nn/layers.py and implement the fc_forward function.
+#############################################################################################################
+
+# Test the fc_forward function
+num_inputs = 2
+input_shape = (4, 5, 6)
+output_dim = 3
+
+input_size = num_inputs * np.prod(input_shape)
+weight_size = output_dim * np.prod(input_shape)
+
+x = np.linspace(-0.1, 0.5, num=input_size).reshape(num_inputs, *input_shape)
+w = np.linspace(-0.2, 0.3, num=weight_size).reshape(np.prod(input_shape), output_dim)
+b = np.linspace(-0.3, 0.1, num=output_dim)
+
+out, _ = fc_forward(x, w, b)
+correct_out = np.array([[ 1.49834967,  1.70660132,  1.91485297],
+                        [ 3.25553199,  3.5141327,   3.77273342]])
+
+# Compare your output with ours. The error should be around 1e-9.
+print('Testing fc_forward function:')
+print('difference: ', rel_error(out, correct_out))
+
+assert rel_error(out, correct_out) < 1e-9
+
+#############################################################################################################
+# TODO (2) Implement the fc_backward function and test your implementation using numeric gradient checking.
+#############################################################################################################
+
+# Test the fc_backward function
+np.random.seed(231)
+x = np.random.randn(10, 2, 3)
+w = np.random.randn(6, 5)
+b = np.random.randn(5)
+dout = np.random.randn(10, 5)
+
+dx_num = eval_numerical_gradient_array(lambda x: fc_forward(x, w, b)[0], x, dout)
+dw_num = eval_numerical_gradient_array(lambda w: fc_forward(x, w, b)[0], w, dout)
+db_num = eval_numerical_gradient_array(lambda b: fc_forward(x, w, b)[0], b, dout)
+
+_, cache = fc_forward(x, w, b)
+dx, dw, db = fc_backward(dout, cache)
+
+# The error should be around 1e-10
+print('Testing fc_backward function:')
+print('dx error: ', rel_error(dx_num, dx))
+print('dw error: ', rel_error(dw_num, dw))
+print('db error: ', rel_error(db_num, db))
+
+assert rel_error(dx_num, dx) < 1e-9
+assert rel_error(dw_num, dw) < 1e-9
+assert rel_error(db_num, db) < 1e-11
+
+##########################################################################################################################################################
+# TODO (3) Implement the forward pass for the ReLU activation function in the relu_forward function and test your implementation using the following.
+##########################################################################################################################################################
+
+# Test the relu_forward function
+
+x = np.linspace(-0.5, 0.5, num=12).reshape(3, 4)
+
+out, _ = relu_forward(x)
+correct_out = np.array([[ 0.,          0.,          0.,          0.,        ],
+                        [ 0.,          0.,          0.04545455,  0.13636364,],
+                        [ 0.22727273,  0.31818182,  0.40909091,  0.5,       ]])
+
+# Compare your output with ours. The error should be around 5e-8
+print('Testing relu_forward function:')
+print('difference: ', rel_error(out, correct_out))
+
+assert rel_error(out, correct_out) < 1e-7
+
+##########################################################################################################################################################
+# TODO (4) Implement the backward pass for the ReLU activation function in the relu_backward function and test your implementation using the following.
+##########################################################################################################################################################
+
+np.random.seed(231)
+x = np.random.randn(10, 10)
+dout = np.random.randn(*x.shape)
+
+dx_num = eval_numerical_gradient_array(lambda x: relu_forward(x)[0], x, dout)
+
+_, cache = relu_forward(x)
+dx = relu_backward(dout, cache)
+
+# The error should be around 3e-12
+print('Testing relu_backward function:')
+print('dx error: ', rel_error(dx_num, dx))
+
+assert rel_error(dx_num, dx) < 1e-11
+
+np.random.seed(231)
+num_classes, num_inputs = 10, 50
+x = 0.001 * np.random.randn(num_inputs, num_classes)
+y = np.random.randint(num_classes, size=num_inputs)
+
+dx_num = eval_numerical_gradient(lambda x: softmax_loss(x, y)[0], x, verbose=False)
+loss, dx = softmax_loss(x, y)
+
+# Test softmax_loss function. Loss should be 2.3 and dx error should be 1e-8
+print('\nTesting softmax_loss:')
+print('loss: ', loss)
+print('dx error: ', rel_error(dx_num, dx))
+
+assert rel_error(dx_num, dx) < 1e-8
+
+##########################################################################################################################################################
+# TODO (5) Train two-layer network on MNIST.
+##########################################################################################################################################################
+
+np.random.seed(231)
+N, D, H, C = 3, 5, 50, 7
+X = np.random.randn(N, D)
+y = np.random.randint(C, size=N)
+
+std = 1e-3
+model = TwoLayerNet(input_dim=D, hidden_dim=H, num_classes=C)
+
+print('Testing test-time forward pass ... ')
+model.params['W1'] = np.linspace(-0.7, 0.3, num=D*H).reshape(D, H)
+model.params['b1'] = np.linspace(-0.1, 0.9, num=H)
+model.params['W2'] = np.linspace(-0.3, 0.4, num=H*C).reshape(H, C)
+model.params['b2'] = np.linspace(-0.9, 0.1, num=C)
+X = np.linspace(-5.5, 4.5, num=N*D).reshape(D, N).T
+scores = model.loss(X)
+correct_scores = np.asarray(
+  [[11.53165108,  12.2917344,   13.05181771,  13.81190102,  14.57198434, 15.33206765,  16.09215096],
+   [12.05769098,  12.74614105,  13.43459113,  14.1230412,   14.81149128, 15.49994135,  16.18839143],
+   [12.58373087,  13.20054771,  13.81736455,  14.43418138,  15.05099822, 15.66781506,  16.2846319 ]])
+scores_diff = np.abs(scores - correct_scores).sum()
+assert scores_diff < 1e-6, 'Problem with test-time forward pass'
+
+print('Testing training loss (no regularization)')
+y = np.asarray([0, 5, 1])
+loss, grads = model.loss(X, y)
+correct_loss = 3.4702243556
+assert abs(loss - correct_loss) < 1e-10, 'Problem with training-time loss'
+
+
+model.reg = 1.0
+loss, grads = model.loss(X, y)
+correct_loss = 26.5948426952
+assert abs(loss - correct_loss) < 1e-10, 'Problem with regularization loss'
+
+for reg in [0.0, 0.7]:
+    print('Running numeric gradient check with reg = ', reg)
+    model.reg = 0
+    loss, grads = model.loss(X, y)
+
+    for name in sorted(grads):
+        f = lambda _: model.loss(X, y)[0]
+        grad_num = eval_numerical_gradient(f, model.params[name], verbose=False)
+        print('%s relative error: %.2e' % (name, rel_error(grad_num, grads[name])))
+
+model = TwoLayerNet()
+solver = Solver(model, data,
+              update_rule='sgd',
+              optim_config={
+                'learning_rate': 1e-3,
+              },
+              lr_decay=0.95,
+              num_epochs=9, batch_size=200,
+              print_every=100)
+solver.train()
+
+
+print('done.')
